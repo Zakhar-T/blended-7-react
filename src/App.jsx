986d3115faa2c8e0f91590db84e6b25e1c1301db
Zakhar-T <zakhar.null@gmail.com>
2025-04-22 21:36:42 +0300

    complete tasks 1,3

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,29 @@ import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
 
+import blogCardData from './data/article.json';
+import forbesData from './data/forbes.json';
+
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          poster={blogCardData.poster}
+          tag={blogCardData.tag}
+          title={blogCardData.title}
+          description={blogCardData.description}
+          name={blogCardData.name}
+          avatar={blogCardData.avatar}
+          postedAt={blogCardData.postedAt}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbesData} />
 
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,5 +1,32 @@
-const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import ForbesListItem from '../ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+
+const ForbesList = ({ list }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(item => {
+          return (
+            <li className={style.item} key={item.id}>
+              <ForbesListItem
+                name={item.name}
+                capital={item.capital}
+                avatar={item.avatar}
+                isIncrease={item.isIncrease}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default ForbesList;
